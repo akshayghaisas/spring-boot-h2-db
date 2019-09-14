3764c68a05f2e7feb145a21fe15f857f3abf412d
Akshay <akshay.ghaisas@nitorinfotech.com>
2019-09-14 22:19:19 +0530

    Added java doc comment

--- src/main/java/com/h2/db/SpringBootH2DbAppApplication.java
+++ src/main/java/com/h2/db/SpringBootH2DbAppApplication.java
@@ -3,6 +3,11 @@ package com.h2.db;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author akshay.ghaisas
+ *
+ */
 @SpringBootApplication
 public class SpringBootH2DbAppApplication {
 
